94cb163ddd349b310973915963d02a6b984aa816
rafaelbatistaroque <maranata_rafa@hotmail.com>
2019-07-29 15:25:23 -0400

    Added Styles fonts

---
+++ lib/style/fontStyle.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+final TextStyle headingStyle =
+    TextStyle(fontSize: 26, fontFamily: "Poppins-Bold");
+final TextStyle categoryStyle =
+    TextStyle(fontSize: 11, fontFamily: "Poppins-Bold");
+final TextStyle lowTextStyle =
+    TextStyle(fontSize: 14, fontFamily: "Poppins-Regular");
+final TextStyle authorTextStyle =
+    TextStyle(fontSize: 10, fontFamily: "Poppins-Light");
+final TextStyle priceTextStyle =
+    TextStyle(fontSize: 16, fontFamily: "Poppins-Bold", color: Colors.green);
+final TextStyle searchBarStyle =
+    TextStyle(fontSize: 18, fontFamily: "Poppins-Light");
+final TextStyle productTitleStyle =
+    TextStyle(fontSize: 16, fontFamily: "Poppins-Bold");
+final TextStyle productSubTitleStyle = TextStyle(fontFamily: "Poppins-Regular");
+final TextStyle bottomBarItemStyle =
+    TextStyle(fontSize: 18, color: Colors.white, fontFamily: "Poppins-Bold");
+final TextStyle speakerDescStyle =
+    TextStyle(fontSize: 16, color: Colors.grey, fontFamily: "Poppins-Light");
