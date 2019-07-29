62ad65f0ca7d63a99707eb521a94e13a9295ce19
rafaelbatistaroque <maranata_rafa@hotmail.com>
2019-07-29 15:33:12 -0400

    Added heading

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:improving_flutter_app_ui_2/style/fontStyle.dart';
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color(0xFFF5F6Fb),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(left: 15, top: 40, right: 10),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text("PlayBook", style: headingStyle),
+              Text("Mais vendidos", style: lowTextStyle),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
