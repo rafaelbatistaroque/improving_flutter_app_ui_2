df25a7e5ec7839c95aa998b4a274ac40345540c7
rafaelbatistaroque <maranata_rafa@hotmail.com>
2019-07-29 15:20:14 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# improving_flutter_app_ui_2
+
+A new Flutter project.
+
+## Getting Started
+
+Project inspired by this layout design:
+
+- [Credits Disign](https://www.uplabs.com/posts/book-reading-app-design)
+![Screen project]()
+
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:improving_flutter_app_ui_2/pages/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
